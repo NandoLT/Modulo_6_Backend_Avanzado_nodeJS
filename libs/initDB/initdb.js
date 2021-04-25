f78de90b6aa8b826f566a21db7ec2f2ac1934fa0
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2021-04-25 23:41:54 +0200

    Modificamos archivo initdb para cargar tambien los datos de usuarios

--- libs/initDB/initdb.js
+++ libs/initDB/initdb.js
@@ -1,18 +1,48 @@
 require('../db-connection');
 const mongoose = require('mongoose')
 const Products = require('../../models/Products')
+const Users = require('../../models/Users')
+const fs = require('fs');
+const { Console } = require('console');
 
-const fs = require('fs')
-const data = fs.readFileSync((__dirname + '/productos.json'), 'utf8')
-const dataParse = JSON.parse(data)
+// const { init } = require('../../models/Products');
 
-async function  dropChargeBd(){
+async function init({dataProducts, dataUsers}) {
+    const responseProducts = await initProducts(dataProducts)
+    const responseUsers = await initUsers(dataUsers)
+    dropChargeBd(responseProducts, responseUsers)
+}
+
+async function initProducts(dataProducts){
+    const producstData = await parseData(dataProducts)
+    return producstData
+}
+
+async function initUsers(dataUsers){
+    const usersData = await parseData(dataUsers)
+    return usersData
+}
+
+async function parseData(dataToParse){
+    const data = fs.readFileSync(dataToParse, 'utf8')
+    const dataParse = JSON.parse(data)
+    return dataParse
+}
+
+async function  dropChargeBd(dataProducts, dataUsers){
     if(Products){
         try {
+            //Products
             await Products.deleteMany({})
-            console.log('Collection deleted')
-            await Products.insertMany(dataParse)
-            console.log("Data inserted")
+            console.log('Collection Products deleted')
+            await Products.insertMany(dataProducts)
+            console.log("Data Products inserted")
+            //Users
+            await Users.deleteMany({})
+            console.log('Collection Users deleted')
+            await Users.insertMany(dataUsers)
+            console.log("Data Users inserted")
+            
             console.log("Closing DB Conecction...")
             mongoose.connection.close()
         } catch (err) {
@@ -20,12 +50,16 @@ async function  dropChargeBd(){
         }
     } else {
         try {
-            await Products.insertMany(dataParse)
-            console.log("Data inserted")
+            //Products
+            await Products.insertMany(dataProducts)
+            console.log("Data Products inserted")
+            //Users
+            await Users.insertMany(dataUsers)
+            console.log("Data Users inserted")
         } catch(err) {
             console.log(err)
         }
     }
 }
 
-dropChargeBd()
+init({dataProducts:(__dirname + '\\productos.json'),dataUsers:(__dirname + '\\users.json')})
---
+++ models/Users.js
@@ -0,0 +1,16 @@
+'use strict'
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema ({
+    email: {
+        type:String, 
+        unique: true
+    },
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+
+module.exports = User;
